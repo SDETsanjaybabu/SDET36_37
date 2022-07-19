173dd41c9153b504c71828be0dffb4d870b2fcf7
sanjaybabu123 <sanjaybabu700@gmail.com>
2022-07-19 11:43:52 +0530

    1st commit

---
+++ my_project/src/test/java/sampleProject/MyClass1Test.java
@@ -0,0 +1,16 @@
+package sampleProject;
+
+import org.testng.annotations.Test;
+
+public class MyClass1Test {
+@Test
+public void sample()
+{
+	System.out.println("hi all");
+}
+@Test
+public void simple()
+{
+	System.out.println("Hello all");
+}
+}
---
+++ my_project/src/test/java/sampleProject/MyClass2.java
@@ -0,0 +1,16 @@
+package sampleProject;
+
+import org.testng.annotations.Test;
+
+public class MyClass2 {
+	@Test
+public void tyss()
+{
+	System.out.println("tyss team");
+}
+	@Test
+	public void rmg()
+	{
+		System.out.println("rmg team");
+	}
+}
