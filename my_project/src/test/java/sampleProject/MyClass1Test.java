a2cbab6c9b5405b32ab5dffbb113a72e8e541e55
sanjaybabu123 <sanjaybabu700@gmail.com>
2022-07-19 22:28:41 +0530

    5t

--- my_project/src/test/java/sampleProject/MyClass1Test.java
+++ my_project/src/test/java/sampleProject/MyClass1Test.java
@@ -1,6 +1,9 @@
 package sampleProject;
 
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class MyClass1Test {
 @Test
@@ -11,9 +14,13 @@ public void sample()
 @Test
 public void simple()
 {
+	
 	String BROWSER=System.getProperty("browser");
 	String URL=System.getProperty("url");
 
+	System.out.println("browser---->"+BROWSER+"<-----url-->"+URL);
+//	WebDriver driver = WebDriverManager.chromedriver().create();
+//	driver.get(URL);
 	
 	System.out.println("Hello all");
 	
